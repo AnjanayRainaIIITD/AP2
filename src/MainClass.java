c5cfde0d9fe276ec835a9a2e5da814972bf40f70
Anjanay Raina <anjanayraina326@gmail.com>
2021-10-17 13:14:00 +0530

    First Commit(2 functions Done)

--- src/MainClass.java
+++ src/MainClass.java
@@ -15,7 +15,7 @@ interface googleClassroom{
 
   public void  logout();
 
-
+    public void printAllFunctions();
 
 
 }
@@ -24,6 +24,11 @@ class student  implements googleClassroom{
 
 
     private String id;
+    public student (){
+        id= "Default";
+
+
+    }
 
     public String getId() {
         return id;
@@ -60,13 +65,32 @@ class student  implements googleClassroom{
 
 
     }
+
+    @Override
+    public void printAllFunctions() {
+
+
+        System.out.println("1. View lecture materials\n" +
+                "2. View assessments\n" +
+                "3. Submit assessment\n" +
+                "4. View grades\n" +
+                "5. View comments\n" +
+                "6. Add comments\n" +
+                "7. Logout");
+
+    }
+
+
 }
 
-class teacher implements googleClassroom{
+class teacher implements googleClassroom {
 
 
-    private HashMap<String  , ArrayList<String>> slide= new HashMap<>();
-    private HashMap<String , String > video = new HashMap<>();
+    private HashMap<String, ArrayList<String>> slide = new HashMap<>();
+    private HashMap<String, String> video = new HashMap<>();
+    private HashMap<String , ArrayList<Integer>> assigment = new HashMap<>();
+
+
 
     private String id;
 
@@ -94,7 +118,13 @@ class teacher implements googleClassroom{
         this.video = video;
     }
 
+    public HashMap<String, ArrayList<Integer>> getAssigment() {
+        return assigment;
+    }
 
+    public void setAssigment(HashMap<String, ArrayList<Integer>> assigment) {
+        this.assigment = assigment;
+    }
 
     @Override
     public void viewLectureMaterial() {
@@ -121,22 +151,158 @@ class teacher implements googleClassroom{
 
         System.out.println("Logging Out!!");
     }
+
+
+    public void printAllFunctions() {
+
+        System.out.println("1. Add class material\n" +
+                "2. Add assessments\n" +
+                "3. View lecture materials\n" +
+                "4. View assessments\n" +
+                "5. Grade assessments\n" +
+                "6. Close assessment\n" +
+                "7. View comments\n" +
+                "8. Add comments\n" +
+                "9. Logout");
+
+
+    }
+
+
+    public void addLectureAndVideo( BufferedReader br ) throws Exception {
+
+        System.out.println("1. Add Lecture Slide\n" +
+                "2. Add Lecture Video");
+
+        int choice  =Integer.parseInt(br.readLine());
+        if(choice ==1){
+
+            System.out.println("Enter topic of slides: ");
+
+            String topic= br.readLine();
+            ArrayList<String > topicSlides= new ArrayList<>();
+            System.out.println("Enter number of slides:");
+            int n=Integer.parseInt(br.readLine());
+            for(int i=0;i<n;i++){
+
+                System.out.println("Content of Slide " +(i+1) +" :");
+
+                topicSlides.add(br.readLine());
+
+
+
+            }
+
+            this.slide.put(topic,topicSlides);
+
+
+
+
+
+        }
+
+        else if(choice ==2){
+
+
+System.out.println("Enter the topic of the video: ");
+String  topic= br.readLine();
+System.out.println("Enter the name of the video :");
+String videoContent =br.readLine();
+this.video.put(topic ,videoContent);
+
+
+
+        }
+
+        else{
+
+            return ;
+
+
+        }
+
+
+    }
+
+
+
+    public void addAssesment(BufferedReader br) throws Exception{
+
+
+
+        System.out.println("1. Add Assignment\n" +
+                "2. Add Quiz");
+
+        int choice  =Integer.parseInt(br.readLine());
+        if(choice ==1){
+
+            System.out.println("Enter problem statement : ");
+
+            String topic= br.readLine();
+
+            System.out.println("Enter the max marks:");
+            int n=Integer.parseInt(br.readLine());
+            ArrayList<Integer> temp = new ArrayList<>();
+            temp.add(n);
+
+
+
+            this.assigment.put(topic,temp);
+
+
+
+
+
+        }
+
+        else if(choice ==2){
+
+
+            System.out.println("Enter the topic of the video: ");
+            String  topic= br.readLine();
+            System.out.println("Enter the name of the video :");
+            String videoContent =br.readLine();
+            this.video.put(topic ,videoContent);
+
+
+
+        }
+
+        else{
+
+            return ;
+
+
+        }
+
+
+
+    }
 }
 
 
 
-public class MainClass  {
-
-    public static void main(String  arg[]) throws Exception {
-        int choice = 1;
-
-        HashMap<String, teacher> t=  new HashMap<>();
-        HashMap<String , student> s= new HashMap<>();
 
 
-        InputStreamReader r=new InputStreamReader(System.in);
-        BufferedReader br=new BufferedReader(r);
 
+public class MainClass {
+
+    public static void main(String arg[]) throws Exception {
+
+
+        HashMap<String, teacher> t = new HashMap<>();
+        t.put("I0" ,new teacher());
+        t.put("I1", new teacher());
+
+        HashMap<String, student> s = new HashMap<>();
+
+        s.put("S0", new student());
+        s.put("S1", new student());
+        s.put("S2", new student());
+
+
+        InputStreamReader r = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(r);
 
 
         while (true) {
@@ -144,68 +310,41 @@ public class MainClass  {
                     "1. Enter as instructor\n" +
                     "2. Enter as student\n" +
                     "3. Exit");
-            int n=Integer.parseInt(br.readLine());
-            if(n==1){
+            int n = Integer.parseInt(br.readLine());
+            if (n == 1) {
 
                 System.out.println("Plesase Enter the ID of instructor: ");
                 String id = br.readLine();
                 teacher temp = t.get(id);
 
-                System.out.println("Welcome : " +id);
-                System.out.println("1. Add Lecture Slide\n" +
-                        "2. Add Lecture Video");
+                System.out.println("Welcome : " + id);
+                temp.printAllFunctions();
+                System.out.println("Enter the function to perform :");
+                switch(Integer.parseInt(br.readLine())){
 
+                    case 1 :
+                        temp.addLectureAndVideo(br);
+                    case 2:
 
-                choice =Integer.parseInt(br.readLine());
-
-                if(choice ==1){
-
-
-                    System.out.println("Enter topic of slides:" );
-                    String topic=br.readLine();
-                    ArrayList<String > topicString  = new ArrayList<>();
-                    System.out.println("Enter number of slides:");
-                    int num= Integer.parseInt(br.readLine());
-                    System.out.println("Enter content of slides :");
-                    for(int i=0;i<num;i++){
-
-                        System.out.println("Content of slide " +i +" : ");
-
-
-
-
-                    }
-
-                    //HashMap<String  , ArrayList<String>> tempSlide =temp.getSlide();
-
-
-
-
-                    
                 }
-
 
             }
 
-            else  if(n==2){
-
-
-                System.out.println("Please enter the ID of the student :");
-                String id=br.readLine();
-
-                student temp =s.get(id);
+            else if(n==2){
 
 
             }
 
             else{
-
+                System.out.println("Logging Out!!");
 
                 break;
+
+
             }
+
+
         }
 
-
     }
-
-    }
+}
